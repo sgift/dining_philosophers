45ff8dd2db51699a16429eee6fd9217ed26319f9
sgift <sebastian.gift@gmail.com>
2015-06-29 02:54:01 +0200

    Implement a basic philosopher
    Basic philosopher that can eat, provide a few example philosophers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+    
+    fn eat(&self) {
+        println!("{} is done eating.", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let philosophers = vec![
+        Philosopher::new("Baruch Spinoza"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Friedrich Nietzsche"),
+        Philosopher::new("Michael Foucault"),
+    ];
+    
+    for p in &philosophers {
+        p.eat();
+    }
 }
