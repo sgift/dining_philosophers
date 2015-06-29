5fdde2292952866a4961947a1e11ab5fbfd38e8d
sgift <sebastian.gift@gmail.com>
2015-06-29 03:10:28 +0200

    Finish dining philosophers problem
    Make one of them left-handed, so he picks the forks in a different order
    from the others

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 use std::thread;
+use std::sync::{Mutex, Arc};
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
     
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+    
         println!("{} is eating.", self.name);
         thread::sleep_ms(1000);
         println!("{} is done eating.", self.name);
@@ -19,17 +31,27 @@ impl Philosopher {
 }
 
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+
     let philosophers = vec![
-        Philosopher::new("Baruch Spinoza"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Marx"),
-        Philosopher::new("Friedrich Nietzsche"),
-        Philosopher::new("Michael Foucault"),
+        Philosopher::new("Baruch Spinoza", 0, 1), 
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Friedrich Nietzsche", 3, 4),
+        Philosopher::new("Michael Foucault", 0, 4),
     ];
     
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+    
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
     
